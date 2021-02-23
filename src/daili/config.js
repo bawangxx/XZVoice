98686120f42d00fe459ec324d844c6c9685d40cc
DESKTOP-HJBOOCD\Administrator <bawangxx@qq.com>
2021-02-23 21:17:44 +0800

    update ...
    update ...

--- src/daili/config.js
+++ src/daili/config.js
@@ -4,11 +4,11 @@ const mainConfig = {
     softVersion: '1.0.5',
     userFolder: "xz_voice", // 用户文件夹名
     proName: 'voice', //项目名称
-    company: '小智软件', //公司名
+    company: '吾爱破解', 
     nshPath: 'src/daili/nsh/installer.nsh', //自定义nsis脚本路径
     baseURL: "http://localhost:1337", // 服务器地址
     isValid: isBuild ? false : true, //是否激活
-    contact: "QQ：525713029", //技术支持
+    contact: "...", //技术支持
     helpUrl: "", //使用帮助链接
     logoName: 'logo.png', // logo
     icon: 'icon.png', //icon
